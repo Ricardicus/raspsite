9344d2078401c53d4a01a90f50f33846d5f73422
Rickard <rickard.hallerback@gmail.com>
2017-03-04 11:47:41 +0000

    Comment on the example_cgi.py

--- cgi/example_cgi.py
+++ cgi/example_cgi.py
@@ -7,7 +7,7 @@
 # Notes:
 # * Every HTTP header will be stored under the os.environ map
 # 	You can use URI encoded data by parsing the os.environ["path"] variable.
-# 	When you end the header section remeber that print outputs \r\n in the end. 
+# 	When you end the header section remeber that print outputs \n in the end. 
 # 	If you enter arguments to the URI, do it like this: 
 # 	if your file is called 'example.py', then request the following: "example.py?[uri-data]"
 # 	The question mark is needed for the server to distinguish between the data and the file path.
@@ -17,5 +17,5 @@
 
 import os
 
-print "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n"
+print "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r"
 print "Hello! You entered the path: " + os.environ["path"]
