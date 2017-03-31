61921d9c9f8cdd114b003f04a355dd4d9e0851f7
Rickard Hallerbäck <rickard_hallerback@hotmail.com>
2017-03-31 20:58:24 +0200

    Url decode added to http_util

--- src/http_util.h
+++ src/http_util.h
@@ -15,6 +15,12 @@ unsigned char *base64_decode(const char *,size_t, size_t *);
 void build_decoding_table(void);
 void base64_cleanup(void); 
 
+/*
+* Function for url decoding found online at:
+* stackoverflow.com: http://stackoverflow.com/questions/2673207/c-c-url-decode-library
+*/
+void urldecode2(char *, const char *)
+
 #endif
 
 #ifdef __cplusplus
