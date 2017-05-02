906ec3de0a7eeb779c07c9da8714bba0888015bf
Rickard Hallerbäck <rickard_hallerback@hotmail.com>
2017-05-02 22:30:22 +0200

    gmail send start

--- etc/mail/send_mail.sh
+++ etc/mail/send_mail.sh
@@ -7,13 +7,15 @@ RECEIVER_MAIL=$2
 SUBJECT_LINE=$3
 PASSWORD=$4
 FILE_NAME=$5
+NICE_NAME=$6
+SENDER_MAIL=$7
 
 if [ "$#" -ne 5 ]; then
-	echo "usage: receiver_name receiver_mail subject password filename"
+	echo "usage: receiver_name receiver_mail subject password filename nicename sendermail"
 	exit 0
 fi
 
-echo "From: 'Rickards RaspberryPi' <ricardicusen@gmail.com>
+echo "From: '$NICE_NAME' <$SENDER_MAIL>
 To: '$RECEIVER_NAME' <$RECEIVER_MAIL>
 Subject: $SUBJECT_LINE
 " > mailheaders_$RECEIVER_MAIL.txt
@@ -24,6 +26,6 @@ rm mailheaders_$RECEIVER_MAIL.txt
 
 # Sending the mail using curl
 echo "sending the content:" && cat $RECEIVER_MAIL.txt && echo " to mail: $RECEIVER_MAIL."
-echo "curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from $RECEIVER_MAIL --mail-rcpt $RECEIVER_MAIL --upload-file $RECEIVER_MAIL.txt --user ricardicusen@gmail.com:$PASSWORD --insecure"
+echo "curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from $RECEIVER_MAIL --mail-rcpt $RECEIVER_MAIL --upload-file $RECEIVER_MAIL.txt --user $SENDER_MAIL:$PASSWORD --insecure"
 
-curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from "$RECEIVER_MAIL" --mail-rcpt "$RECEIVER_MAIL" --upload-file fil.txt --user "ricardicusen@gmail.com:$PASSWORD" --insecure
+curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from "$RECEIVER_MAIL" --mail-rcpt "$RECEIVER_MAIL" --upload-file fil.txt --user "$SENDER_MAIL:$PASSWORD" --insecure
